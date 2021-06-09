c18e176c732e0f68fb62169d79129d581a055102
cchaiyatad <chaiyatad@gmail.com>
2021-06-09 21:03:27 +0700

    Edit: edit token in run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
 filePath=../data/fng.1000.csv.rot128
-omisePublicKey="pkey_test_5o0y2isu7yq96qbfrz3"
-omiseSecretKey="skey_test_5o0y2isu7yd5ruh6vri"
+omisePublicKey="pkey_test_5o4fc4wux9ec01nmmot"
+omiseSecretKey="skey_test_5o4fc5etrop6k804sjv"
 debug="false"
 
 go build -o build/app || exit 1
 cd build
+
 ./app -f $filePath \
     -pk $omisePublicKey \
     -sk $omiseSecretKey \
-    -n 4 \
+    -n 3 \
     -t 3 \
     -d=$debug
+
 cd ..
